57a2e0c7c82b0f9771d132526672e8c737ad4d43
songxing10000 <songxing10000@live.cn>
2021-04-16 11:56:41 +0800

    pod install

---
+++ Pods/Target Support Files/DragAndDropTableView/DragAndDropTableView-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DragAndDropTableView.h"
+#import "UIView+Snapshot.h"
+
+FOUNDATION_EXPORT double DragAndDropTableViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char DragAndDropTableViewVersionString[];
+
---
+++ Pods/Target Support Files/Pods-ning/Pods-ning-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ning : NSObject
+@end
+@implementation PodsDummy_Pods_ning
+@end
